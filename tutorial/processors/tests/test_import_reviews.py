620d3418f5b03eb8016511f1910151fb9371c3d6
Sven Schubert <svschub@gmx.de>
2015-04-16 16:07:15 +0200

    change files that should have been changed in the previous commit

--- tutorial/processors/tests/test_import_reviews.py
+++ tutorial/processors/tests/test_import_reviews.py
@@ -5,3 +5,9 @@ class TestImportReviews(unittest.TestCase):
     def test_init(self):
         r = processors.ImportReviews()
         assert r
+
+    def test_run(self):
+        r = processors.ImportReviews()
+        r.datafile = "movies.json"
+        r.run();
+
